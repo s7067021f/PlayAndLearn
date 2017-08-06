82ee150d4a64c59963561a58464a6abf10278b8b
Mike Sutherland <s7067021f@gmail.com>
2017-08-06 07:09:34 +0000

    adding AssertPlay.java

---
+++ src/main/java/com/mike/AssertPlay.java
@@ -0,0 +1,14 @@
+package com.mike;
+
+class Setup{
+    int a = 10;
+    assert (a < 20) : "A is not less than 20. A is " + a;
+}
+
+public class AssertPlay{
+    public static void main(String[] args){
+        AssertPlay assertPlay = new AssertPlay();
+        
+    }
+    
+}
